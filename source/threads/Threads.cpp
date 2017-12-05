129b99ae33c79bb3d8431166bc897cf621cceabb
yhcha <carhero45@gmail.com>
2017-12-05 15:33:22 +0900

    uart 추가

--- source/threads/Threads.cpp
+++ source/threads/Threads.cpp
@@ -11,6 +11,7 @@
 #include <pthread.h>
 
 #include "../console/console.h"
+#include "../drivers/uart/Example_uart.h"
 
 void* foo(void* ptr)
 {
@@ -38,26 +39,28 @@ int Init_CreateThreads(void)
   int  iret1 = 0;
   int  iret2 = 0;
   int  iret3 = 0;
+  int  iret4 = 0;
   int  idata = 1;
 
   pthread_t first;     // spawn new thread that calls foo()
   pthread_t second;    // spawn new thread that calls bar(0)
   pthread_t third;     // spawn new thread that calls bar(0)
+  pthread_t fourth;     // spawn new thread that calls bar(0)
 
   printf("main, foo and bar now execute concurrently...\n");
   iret1 = pthread_create( &first, NULL, foo, NULL);
   iret2 = pthread_create( &second, NULL, bar, (void*)&idata);
-  iret2 = pthread_create( &third, NULL, Console_TaskMain, NULL);
+  iret3 = pthread_create( &third, NULL, Console_TaskMain, NULL);
+  iret4 = pthread_create( &fourth, NULL, uart_TaskMain, NULL);
 
   // synchronize threads:
-  pthread_join(first, NULL);                // pauses until first finishes
-  pthread_join(second, NULL);               // pauses until second finishes
+  //pthread_join(first, NULL);                // pauses until first finishes
+  //pthread_join(second, NULL);               // pauses until second finishes
 
   printf("Thread 1 returns: %d\n",iret1);
   printf("Thread 2 returns: %d\n",iret2);
-  printf("Thread 3 returns: %d\n",iret2);
-
-  printf("foo and bar completed.\n");
+  printf("Thread 3 returns: %d\n",iret3);
+  printf("Thread 4 returns: %d\n",iret4);
 
   return 0;
 }
